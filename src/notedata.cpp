4816c3f91566a588f938446413b4396f6159dcc5
rion <rion4ik@gmail.com>
2010-08-03 13:12:29 +0000

    * cut down long titles. * small fix in ui file
    git-svn-id: https://qtnote.googlecode.com/svn/trunk@51 c9899a76-f8cd-11dd-87c9-111d11f6bf44

--- src/notedata.cpp
+++ src/notedata.cpp
@@ -41,5 +41,8 @@ void NoteData::setText(const QString &text)
 {
 	sText = text.trimmed();
 	sTitle = sText.section('\n', 0, 0).trimmed();
+	if (sTitle.length() > 48) {
+		sTitle = sTitle.mid(0, 48) + "...";
+	}
 }
 
