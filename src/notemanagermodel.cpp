35061f8ccfa1004dd91590ad9714cbe49f6c2027
rion <rion4ik@gmail.com>
2011-03-18 11:59:32 +0000

    some progress on notemanager
    git-svn-id: https://qtnote.googlecode.com/svn/trunk@61 c9899a76-f8cd-11dd-87c9-111d11f6bf44

--- src/notemanagermodel.cpp
+++ src/notemanagermodel.cpp
@@ -1,20 +1,105 @@
+/*
+QtNote - Simple note-taking application
+Copyright (C) 2010 Ili'nykh Sergey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contacts:
+E-Mail: rion4ik@gmail.com XMPP: rion@jabber.ru
+*/
+
+#include <QStringList>
+
 #include "notemanagermodel.h"
 #include "notemanager.h"
 
-NoteManagerModel::NoteManagerModel(QObject *parent) :
-    QAbstractItemModel(parent)
+
+class NMMItem
 {
+public:
+	enum Type {
+		Storage,
+		Note
+	};
+
+	NMMItem(const StorageItem &storage)
+		: parent(0)
+		, type(Storage)
+		, title(storage.storage->titleName())
+		, id(storage.storage->systemName())
+	{
+		foreach (const NoteListItem &note, storage.storage->noteList()) {
+			children.append(new NMMItem(note, this));
+		}
+	}
+
+	NMMItem(const NoteListItem &note, NMMItem *parent)
+		: parent(parent)
+		, type(Note)
+		, title(note.title)
+		, id(note.id)
+	{
+
+	}
+
+	~NMMItem()
+	{
+		qDeleteAll(children);
+	}
+
+	NMMItem *parent;
+	Type type;
+	QList<NMMItem *> children;
+	QString title;
+	QString id;
+};
+
+void debug(const QString &prefix, NMMItem *item)
+{
+	qDebug("%s: %s(%s)", qPrintable(prefix), item->type == NMMItem::Storage? "Storage":"Note", qPrintable(item->title));
+}
+
+NoteManagerModel::NoteManagerModel(QObject *parent)
+	: QAbstractItemModel(parent)
+{
+	foreach (const StorageItem &s, NoteManager::instance()->storages()) {
+		storages.append(new NMMItem(s));
+	}
+}
+
+NoteManagerModel::~NoteManagerModel()
+{
+	qDeleteAll(storages);
 }
 
 QModelIndex NoteManagerModel::index( int row, int column,
 									 const QModelIndex & parent ) const
 {
-	if (parent.isValid()) {
+	if (!hasIndex(row, column, parent)) {
+		return QModelIndex();
+	}
 
-	} else {
-		if (column == 0 && row < NoteManager::instance()->storages().size()) {
-			return createIndex(row, column,
-							   qHash(NoteManager::instance()->storages().keys()[row]));
+	if (column < 1) {
+		if (parent.isValid()) {
+			NMMItem *parentItem = static_cast<NMMItem *>(parent.internalPointer());
+			if (parentItem->type == NMMItem::Storage && row < parentItem->children.count()) {
+				return createIndex(row, column, parentItem->children[row]);
+			}
+		} else {
+			if (row < storages.count()) {
+				return createIndex(row, column, storages[row]);
+			}
 		}
 	}
 	return QModelIndex();
@@ -22,30 +107,40 @@ QModelIndex NoteManagerModel::index( int row, int column,
 
 QModelIndex NoteManagerModel::parent( const QModelIndex & index ) const
 {
+	if (index.isValid()) {
+		NMMItem *item = static_cast<NMMItem *>(index.internalPointer());
+		int n = storages.indexOf(item);
+		if (n == -1) { // its valid note
+			return createIndex(storages.indexOf(item->parent), 0, item->parent);
+		}
+	}
 	return QModelIndex();
 }
 
 int NoteManagerModel::rowCount( const QModelIndex & parent ) const
 {
 	if (parent.isValid()) {
-
+		return static_cast<NMMItem *>(parent.internalPointer())->children.count();
 	} else {
-		return NoteManager::instance()->storages().size();
+		return storages.size();
 	}
 	return 0;
 }
 
-int NoteManagerModel::columnCount( const QModelIndex & parent ) const
+int NoteManagerModel::columnCount( const QModelIndex &parent) const
 {
+	if (parent.isValid() && static_cast<NMMItem *>(parent.internalPointer())->type == NMMItem::Note) { // note has no children so 0 columns
+		return 0;
+	}
 	return 1;
 }
 
 QVariant NoteManagerModel::data( const QModelIndex & index, int role ) const
 {
-	if (role == Qt::DisplayRole && index.isValid()) {
-		QList<StorageItem> storageItems = NoteManager::instance()->storages().values();
-		if (index.row() < storageItems.size() && index.column() == 0) {
-			return storageItems[index.row()].storage->titleName();
+	if (index.isValid()) {
+		NMMItem *item = static_cast<NMMItem *>(index.internalPointer());
+		if (role == Qt::DisplayRole) {
+			return item->title;
 		}
 	}
 	return QVariant();
--- src/notemanagermodel.h
+++ src/notemanagermodel.h
@@ -1,13 +1,38 @@
+/*
+QtNote - Simple note-taking application
+Copyright (C) 2010 Ili'nykh Sergey
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Contacts:
+E-Mail: rion4ik@gmail.com XMPP: rion@jabber.ru
+*/
+
 #ifndef NOTEMANAGERMODEL_H
 #define NOTEMANAGERMODEL_H
 
 #include <QAbstractItemModel>
+
+class NoteStorage;
+class NMMItem;
 
 class NoteManagerModel : public QAbstractItemModel
 {
     Q_OBJECT
 public:
     explicit NoteManagerModel(QObject *parent = 0);
+	~NoteManagerModel();
 	QModelIndex index( int row, int column, const QModelIndex & parent = QModelIndex() ) const;
 	QModelIndex parent( const QModelIndex & index ) const;
 	int rowCount( const QModelIndex & parent = QModelIndex() ) const;
@@ -18,6 +43,10 @@ signals:
 
 public slots:
 
+private:
+	QList<NMMItem*> storages;
+	//QList<NoteStorage*> storages;
+	//QHash<QString, QList<NoteListItem> > notes; // storageSystemName => Array<noteId>
 };
 
 #endif // NOTEMANAGERMODEL_H
