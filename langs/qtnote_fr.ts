dbd1e5854e2312b4e4a1adc7747668a5ad9c8af0
rion <rion4ik@gmail.com>
2013-07-06 23:50:23 +0600

    Updated translation tab

--- langs/qtnote_fr.ts
+++ langs/qtnote_fr.ts
@@ -1,52 +1,30 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
-<defaultcodec>UTF-8</defaultcodec>
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.0">
 <context>
     <name>AboutDlg</name>
     <message>
         <location filename="../src/aboutdlg.ui" line="14"/>
         <source>About QtNote</source>
-        <translation>О QtNote</translation>
+        <translation>À propos de QtNote</translation>
     </message>
     <message>
-        <location filename="../src/aboutdlg.ui" line="49"/>
-        <source>About</source>
-        <translation>О Программе</translation>
-    </message>
-    <message>
-        <location filename="../src/aboutdlg.ui" line="63"/>
+        <location filename="../src/aboutdlg.ui" line="58"/>
         <source>Simpe and usable note taking application</source>
-        <translation>Простые и удобные заметки</translation>
+        <translation>Application de prise de note simple et efficace</translation>
     </message>
     <message>
-        <location filename="../src/aboutdlg.ui" line="75"/>
+        <location filename="../src/aboutdlg.ui" line="86"/>
         <source>License: GPL-3</source>
-        <translation>Лицензия: GPL-3</translation>
+        <translation>Licence : GPL-3</translation>
     </message>
     <message>
-        <location filename="../src/aboutdlg.ui" line="87"/>
+        <location filename="../src/aboutdlg.ui" line="98"/>
         <source>Author: Il&apos;inykh Sergey</source>
-        <translation>Автор: Ильиных Сергей</translation>
+        <translation>Auteur : Il&apos;inykh Sergey</translation>
     </message>
     <message>
-        <location filename="../src/aboutdlg.ui" line="120"/>
-        <source>Translations</source>
-        <translation>Переводы</translation>
-    </message>
-    <message>
-        <location filename="../src/aboutdlg.ui" line="132"/>
-        <source>Russian:
-  Il&apos;inykh Sergey &lt;rion4ik@gmail.com&gt;
-
-Vietnamese:
-  Phan Anh
-</source>
-        <translation>Русский:
-  Ильиных Сергей &lt;rion4ik@gmail.com&gt;
-
-Вьетнамский:
-  Phan Anh</translation>
+        <location filename="../src/aboutdlg.ui" line="117"/>
+        <source>www: &lt;a href=&quot;http://qtnote.googlecode.com/&quot;&gt;http://qtnote.googlecode.com&lt;/a&gt;</source>
+        <translation>www: &lt;a href=&quot;http://qtnote.googlecode.com/&quot;&gt;http://qtnote.googlecode.com&lt;/a&gt;</translation>
     </message>
 </context>
 <context>
@@ -54,17 +32,17 @@ Vietnamese:
     <message>
         <location filename="../src/notedialog.ui" line="31"/>
         <source>Copy note to clipboard</source>
-        <translation>Скопировать заметку в буфер обмена</translation>
+        <translation>Copier la note vers le presse-papier</translation>
     </message>
     <message>
         <location filename="../src/notedialog.ui" line="61"/>
         <source>Delete note</source>
-        <translation>Удалить заметку</translation>
+        <translation>Supprimer la note</translation>
     </message>
     <message>
         <location filename="../src/notedialog.cpp" line="138"/>
         <source>[No Title]</source>
-        <translation>[Нет заголовка]</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -72,21 +50,17 @@ Vietnamese:
     <message>
         <location filename="../src/notemanagerdlg.ui" line="52"/>
         <source>Close</source>
-        <translation>Закрыть</translation>
+        <translation>Fermer</translation>
     </message>
     <message>
         <location filename="../src/notemanagerdlg.cpp" line="17"/>
         <source>Note Manager (%1)</source>
-        <translation>Менеджер заметок (%1)</translation>
+        <translation>Gestionnaire de notes (%1)</translation>
     </message>
     <message numerus="yes">
         <location filename="../src/notemanagerdlg.cpp" line="17"/>
         <source>%n notes</source>
-        <translation>
-            <numerusform>%n заметка</numerusform>
-            <numerusform>%n заметки</numerusform>
-            <numerusform>%n заметок</numerusform>
-        </translation>
+        <translation><numerusform>%n note</numerusform><numerusform>%n notes</numerusform></translation>
     </message>
 </context>
 <context>
@@ -94,7 +68,7 @@ Vietnamese:
     <message>
         <location filename="../src/notemanagerview.cpp" line="18"/>
         <source>Delete</source>
-        <translation>Удалить</translation>
+        <translation>Supprimer</translation>
     </message>
 </context>
 <context>
@@ -102,27 +76,27 @@ Vietnamese:
     <message>
         <location filename="../src/optionsdlg.ui" line="14"/>
         <source>QtNote Options</source>
-        <translation>QtNote Настройки</translation>
+        <translation>Options de QtNote</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="24"/>
         <source>Storage Priority</source>
-        <translation>Приоритет хранилищ</translation>
+        <translation>Priorité de stockage</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="43"/>
         <source>Delete notes with confirmation</source>
-        <translation>Удалять заметки с подтверждением</translation>
+        <translation>Supprimer les notes après confirmation</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="50"/>
         <source>Start with system</source>
-        <translation>Запускать вместе с системой</translation>
+        <translation>Lancer au démarrage du système</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="59"/>
         <source>Amount of notes to show in the tray menu</source>
-        <translation>Количество заметок в меню трея</translation>
+        <translation>Nombre de notes à afficher dans la barre des tâches</translation>
     </message>
 </context>
 <context>
@@ -130,7 +104,7 @@ Vietnamese:
     <message>
         <location filename="../src/ptfstorage.cpp" line="55"/>
         <source>Plain Text Storage</source>
-        <translation>Хранилище текстовых файлов</translation>
+        <translation>Stockage plein texte</translation>
     </message>
 </context>
 <context>
@@ -138,7 +112,7 @@ Vietnamese:
     <message>
         <location filename="../src/main.cpp" line="95"/>
         <source>no one of note storages is accessible. can&apos;t continue..</source>
-        <translation>Ни одно из хранилищ недоступно. Продолжение невозможно..</translation>
+        <translation>Aucun stockage de note accessible. Impossible de continuer...</translation>
     </message>
 </context>
 <context>
@@ -146,7 +120,7 @@ Vietnamese:
     <message>
         <location filename="../src/tomboystorage.cpp" line="78"/>
         <source>Tomboy Storage</source>
-        <translation>Хранилище Tomboy</translation>
+        <translation>Stockage Tomboy</translation>
     </message>
 </context>
 <context>
@@ -154,37 +128,37 @@ Vietnamese:
     <message>
         <location filename="../src/mainwidget.cpp" line="39"/>
         <source>&amp;Quit</source>
-        <translation>&amp;Выход</translation>
+        <translation>&amp;Quitter</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="40"/>
         <source>&amp;New</source>
-        <translation>&amp;Новая</translation>
+        <translation>&amp;Nouvelle</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="41"/>
         <source>&amp;About</source>
-        <translation>&amp;О Программе</translation>
+        <translation>&amp;À propos</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="42"/>
         <source>&amp;Options</source>
-        <translation>Н&amp;астройки</translation>
+        <translation>&amp;Options</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="43"/>
         <source>&amp;Note Manager</source>
-        <translation>&amp;Менеджер Заметок</translation>
+        <translation>&amp;Gestionnaire de notes</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="216"/>
         <source>Deletion confirmation</source>
-        <translation>Подтверждение удаления</translation>
+        <translation>Confirmation de la suppression</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="217"/>
         <source>Are you sure want to delete this note?</source>
-        <translation>Вы действительно хотите удалить эту заметку?</translation>
+        <translation>Êtes-vous certain de vouloir supprimer cette note ?</translation>
     </message>
 </context>
 </TS>
--- langs/qtnote_vi.ts
+++ langs/qtnote_vi.ts
@@ -1,52 +1,48 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="ru_RU">
-<defaultcodec>UTF-8</defaultcodec>
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="vi" version="2.0">
 <context>
     <name>AboutDlg</name>
     <message>
         <location filename="../src/aboutdlg.ui" line="14"/>
         <source>About QtNote</source>
-        <translation>О QtNote</translation>
+        <translation>Dịch bởi PA</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.ui" line="49"/>
         <source>About</source>
-        <translation>О Программе</translation>
+        <translation>Thông tin</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.ui" line="63"/>
         <source>Simpe and usable note taking application</source>
-        <translation>Простые и удобные заметки</translation>
+        <translation>Ứng dụng tạo các ghi chú đơn giản và dễ sử dụng</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.ui" line="75"/>
         <source>License: GPL-3</source>
-        <translation>Лицензия: GPL-3</translation>
+        <translation>Giấy phép phát hành: GPL-3</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.ui" line="87"/>
         <source>Author: Il&apos;inykh Sergey</source>
-        <translation>Автор: Ильиных Сергей</translation>
+        <translation>Tác giả: Il&apos;inykh Sergey</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.ui" line="120"/>
         <source>Translations</source>
-        <translation>Переводы</translation>
+        <translation>Dịch thuật</translation>
     </message>
     <message>
-        <location filename="../src/aboutdlg.ui" line="132"/>
-        <source>Russian:
-  Il&apos;inykh Sergey &lt;rion4ik@gmail.com&gt;
-
-Vietnamese:
-  Phan Anh
-</source>
-        <translation>Русский:
-  Ильиных Сергей &lt;rion4ik@gmail.com&gt;
-
-Вьетнамский:
-  Phan Anh</translation>
+        <location filename="../src/aboutdlg.ui" line="126"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:'Sans'; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Russian:&lt;br /&gt;Il&apos;inykh Sergey (rion4ik@gmail.com)&lt;br /&gt;&lt;br /&gt;French:&lt;br /&gt;&lt;br /&gt;Vietnamese:&lt;br /&gt;Phan Anh&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Russian:&lt;br /&gt;Il&apos;inykh Sergey (rion4ik@gmail.com)&lt;br /&gt;&lt;br /&gt;French:&lt;br /&gt;&lt;br /&gt;Vietnamese:&lt;br /&gt;Phan Anh&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 <context>
@@ -54,17 +50,17 @@ Vietnamese:
     <message>
         <location filename="../src/notedialog.ui" line="31"/>
         <source>Copy note to clipboard</source>
-        <translation>Скопировать заметку в буфер обмена</translation>
+        <translation>Sao chép ghi chú vào bộ nhớ ảo</translation>
     </message>
     <message>
         <location filename="../src/notedialog.ui" line="61"/>
         <source>Delete note</source>
-        <translation>Удалить заметку</translation>
+        <translation>Xóa ghi chú</translation>
     </message>
     <message>
         <location filename="../src/notedialog.cpp" line="138"/>
         <source>[No Title]</source>
-        <translation>[Нет заголовка]</translation>
+        <translation>[Chưa đặt tiêu đề]</translation>
     </message>
 </context>
 <context>
@@ -72,21 +68,17 @@ Vietnamese:
     <message>
         <location filename="../src/notemanagerdlg.ui" line="52"/>
         <source>Close</source>
-        <translation>Закрыть</translation>
+        <translation>Đóng lại</translation>
     </message>
     <message>
         <location filename="../src/notemanagerdlg.cpp" line="17"/>
         <source>Note Manager (%1)</source>
-        <translation>Менеджер заметок (%1)</translation>
+        <translation>Quản lý ghi chú (%1)</translation>
     </message>
     <message numerus="yes">
         <location filename="../src/notemanagerdlg.cpp" line="17"/>
         <source>%n notes</source>
-        <translation>
-            <numerusform>%n заметка</numerusform>
-            <numerusform>%n заметки</numerusform>
-            <numerusform>%n заметок</numerusform>
-        </translation>
+        <translation><numerusform>%n ghi chú</numerusform></translation>
     </message>
 </context>
 <context>
@@ -94,7 +86,7 @@ Vietnamese:
     <message>
         <location filename="../src/notemanagerview.cpp" line="18"/>
         <source>Delete</source>
-        <translation>Удалить</translation>
+        <translation>Xóa</translation>
     </message>
 </context>
 <context>
@@ -102,27 +94,27 @@ Vietnamese:
     <message>
         <location filename="../src/optionsdlg.ui" line="14"/>
         <source>QtNote Options</source>
-        <translation>QtNote Настройки</translation>
+        <translation>Tùy chọn chương trình</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="24"/>
         <source>Storage Priority</source>
-        <translation>Приоритет хранилищ</translation>
+        <translation>Ưu tiên lưu trữ</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="43"/>
         <source>Delete notes with confirmation</source>
-        <translation>Удалять заметки с подтверждением</translation>
+        <translation>Xác nhận xóa ghi chú</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="50"/>
         <source>Start with system</source>
-        <translation>Запускать вместе с системой</translation>
+        <translation>Khởi động với hệ thống</translation>
     </message>
     <message>
         <location filename="../src/optionsdlg.ui" line="59"/>
         <source>Amount of notes to show in the tray menu</source>
-        <translation>Количество заметок в меню трея</translation>
+        <translation>Số lượng ghi chú hiển thị ở khay hệ thống</translation>
     </message>
 </context>
 <context>
@@ -130,7 +122,7 @@ Vietnamese:
     <message>
         <location filename="../src/ptfstorage.cpp" line="55"/>
         <source>Plain Text Storage</source>
-        <translation>Хранилище текстовых файлов</translation>
+        <translation>Lưu trữ văn bản thông thường</translation>
     </message>
 </context>
 <context>
@@ -138,7 +130,7 @@ Vietnamese:
     <message>
         <location filename="../src/main.cpp" line="95"/>
         <source>no one of note storages is accessible. can&apos;t continue..</source>
-        <translation>Ни одно из хранилищ недоступно. Продолжение невозможно..</translation>
+        <translation>không thể truy cập bất kỳ ghi chú nào. không thể tiếp tục...</translation>
     </message>
 </context>
 <context>
@@ -146,7 +138,7 @@ Vietnamese:
     <message>
         <location filename="../src/tomboystorage.cpp" line="78"/>
         <source>Tomboy Storage</source>
-        <translation>Хранилище Tomboy</translation>
+        <translation>Lưu trữ Tomboy</translation>
     </message>
 </context>
 <context>
@@ -154,37 +146,37 @@ Vietnamese:
     <message>
         <location filename="../src/mainwidget.cpp" line="39"/>
         <source>&amp;Quit</source>
-        <translation>&amp;Выход</translation>
+        <translation>&amp;Thoát</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="40"/>
         <source>&amp;New</source>
-        <translation>&amp;Новая</translation>
+        <translation>&amp;Tạo mới</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="41"/>
         <source>&amp;About</source>
-        <translation>&amp;О Программе</translation>
+        <translation>&amp;Thông tin</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="42"/>
         <source>&amp;Options</source>
-        <translation>Н&amp;астройки</translation>
+        <translation>T&amp;ùy chọn</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="43"/>
         <source>&amp;Note Manager</source>
-        <translation>&amp;Менеджер Заметок</translation>
+        <translation>Q&amp;uản lý ghi chú</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="216"/>
         <source>Deletion confirmation</source>
-        <translation>Подтверждение удаления</translation>
+        <translation>Xác nhận xóa</translation>
     </message>
     <message>
         <location filename="../src/mainwidget.cpp" line="217"/>
         <source>Are you sure want to delete this note?</source>
-        <translation>Вы действительно хотите удалить эту заметку?</translation>
+        <translation>Bạn có muốn xóa ghi chú này hay không?</translation>
     </message>
 </context>
 </TS>
