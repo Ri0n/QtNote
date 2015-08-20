2c10a24bd904ca8ff7c63199f604b36d58297171
rion <rion4ik@gmail.com>
2015-08-20 08:32:52 +0500

    Change font and color for plugin item in options dialog depending on current status. fixes #45

--- libqtnote/optionsplugins.cpp
+++ libqtnote/optionsplugins.cpp
@@ -187,6 +187,26 @@ public:
                 }
                 return ret;
             }
+
+            case Qt::FontRole:
+            {
+                QFont f; // application default font. may by not what we expect
+                PluginManager::LoadStatus status = qtnote->pluginManager()->loadStatus(pluginId);
+                if (status == PluginManager::LS_Initialized) {
+                    f.setBold(true);
+                }
+                return f;
+            }
+
+            case Qt::ForegroundRole:
+            {
+                QColor color = qApp->palette().color(QPalette::Foreground); // mey be not what we expect
+                PluginManager::LoadStatus status = qtnote->pluginManager()->loadStatus(pluginId);
+                if (!(status == PluginManager::LS_Initialized || status == PluginManager::LS_Initialized)) {
+                    color.setAlpha(128);
+                }
+                return color;
+            }
             }
         } else if (index.column() == 1) { // version
             if (role == Qt::DisplayRole) {
