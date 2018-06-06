d7bfcf35f6ee2c63fa5d323663bf45746bfcd031
Sergey Ilinykh <rion4ik@gmail.com>
2018-06-06 19:13:50 +0300

    Updated dependencies to be compatible with recent debian systems

--- admin/debbuild.sh
+++ admin/debbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install libkf5globalaccel-dev libkf5windowsystem-dev libkf5notifications-dev
+sudo apt-get install libkf5globalaccel-dev libkf5windowsystem-dev libkf5notifications-dev git-buildpackage debhelper cdbs qtchooser pkg-config libqt5x11extras5-dev libhunspell-dev
 
 export QT_SELECT=5
 exec gbp buildpackage --git-ignore-branch --git-ignore-new -us -uc -j5
