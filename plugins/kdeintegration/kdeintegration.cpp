4ddc53f2872a5857d54c01eec969177ead424adc
Sergey Ilinykh <rion4ik@gmail.com>
2018-11-16 01:10:03 +0300

    Added another session value to kde plugin (works on suse+plasma5)

--- plugins/kdeintegration/kdeintegration.cpp
+++ plugins/kdeintegration/kdeintegration.cpp
@@ -44,7 +44,8 @@ PluginMetadata KDEIntegration::metadata()
     md.minVersion = 0x020300; // minimum compatible version of QtNote
     md.maxVersion = QTNOTE_VERSION; // maximum compatible version of QtNote
     md.homepage = QUrl("http://ri0n.github.io/QtNote");
-    md.extra.insert("de", QStringList() << "KDE-4" << "kde-plasma" << "/usr/share/xsessions/plasma" << "plasma");
+    md.extra.insert("de", QStringList() << "KDE-4" << "kde-plasma" << "/usr/share/xsessions/plasma"
+                    << "plasma" << "/usr/share/xsessions/plasma5");
     return md;
 }
 
