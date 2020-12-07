afe35c8c852478e7f207d9fbc3807bc3c7211277
Sergey Ilinykh <rion4ik@gmail.com>
2020-12-07 13:33:26 +0300

    Install qtnote

--- cmake/modules/QtNoteMacro.cmake
+++ cmake/modules/QtNoteMacro.cmake
@@ -24,7 +24,12 @@ endif()
 macro(add_qtnote_plugin name description)
     cmake_minimum_required(VERSION 3.10.0)
     project(qtnote_plugin_${name} VERSION ${QTNOTE_VERSION} LANGUAGES CXX)
-    option(QTNOTE_ENABLE_${name} "Enable QtNote plugin: ${description}")
+    option(QTNOTE_ENABLE_${name} "Enable QtNote plugin: ${description}" ON)
+
+    if (NOT QTNOTE_ENABLE_${name})
+        return()
+    endif()
+
     set(CMAKE_AUTOMOC ON)
     set(CMAKE_AUTORCC ON)
     set(CMAKE_AUTOUIC ON)
