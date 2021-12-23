987c0989c9f218ac54a38d22931b2085fa3cbe67
Sergei Ilinykh <rion4ik@gmail.com>
2021-12-23 09:26:46 +0300

    Auto debian release on changelog version mismatch

--- admin/debbuild.sh
+++ admin/debbuild.sh
@@ -3,5 +3,7 @@
 sudo apt-get install libkf5globalaccel-dev libkf5windowsystem-dev libkf5notifications-dev git-buildpackage debhelper \
   pkg-config dh-cmake dh-sequence-cmake libqt5x11extras5-dev libhunspell-dev
 
+$(dirname $0)/debrelease
+
 export QT_SELECT=5
 exec gbp buildpackage --git-ignore-branch --git-ignore-new -us -uc -j10
