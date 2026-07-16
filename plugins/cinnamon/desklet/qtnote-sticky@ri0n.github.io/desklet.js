433084e43008088247470095d1db850f3d44b0c0
Sergei Ilinykh <rion4ik@gmail.com>
2026-07-17 01:07:49 +0300

    cinnamon: resizeable sticky notes

--- plugins/cinnamon/desklet/qtnote-sticky@ri0n.github.io/desklet.js
+++ plugins/cinnamon/desklet/qtnote-sticky@ri0n.github.io/desklet.js
@@ -1,12 +1,17 @@
 const Desklet = imports.ui.desklet;
+const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
+const Main = imports.ui.main;
 const Pango = imports.gi.Pango;
 const PopupMenu = imports.ui.popupMenu;
+const Settings = imports.ui.settings;
 const St = imports.gi.St;
 
 const BUS_NAME = 'com.github.ri0n.QtNote';
 const OBJECT_PATH = '/QtNote';
+const GRIP_TRANSLATION_X = 0;
+const GRIP_TRANSLATION_Y = 0;
 
 const QTNOTE_IFACE_XML = `
 <node>
@@ -37,22 +42,67 @@ class QtNoteStickyDesklet extends Desklet.Desklet {
         this._stickyId = '';
         this._signalId = 0;
         this._ownerSignalId = 0;
+        this.width = 300;
+        this.height = 180;
 
-        this._box = new St.BoxLayout({
-            vertical: true,
+        this._settings = new Settings.DeskletSettings(this, metadata.uuid, this.instance_id);
+        this._settings.bind('width', 'width', Lang.bind(this, this._applySize));
+        this._settings.bind('height', 'height', Lang.bind(this, this._applySize));
+
+        this._resizeEventActor = new Clutter.Actor({width: 0, height: 0, reactive: true});
+        Main.uiGroup.add_actor(this._resizeEventActor);
+        this._resizeEventId = this._resizeEventActor.connect('event', Lang.bind(this, this._handleResizeEvent));
+
+        this._box = new St.Widget({
             reactive: true,
+            layout_manager: new Clutter.BinLayout({
+                x_align: Clutter.BinAlignment.FILL,
+                y_align: Clutter.BinAlignment.FILL,
+            }),
             style_class: 'qtnote-sticky',
+        });
+        this._content = new St.BoxLayout({
+            vertical: true,
+            x_expand: true,
+            y_expand: true,
+            style_class: 'qtnote-sticky-content',
         });
         this._title = new St.Label({style_class: 'qtnote-sticky-title'});
         this._title.clutter_text.ellipsize = Pango.EllipsizeMode.END;
-        this._box.add_actor(this._title);
+        this._content.add_actor(this._title);
 
-        this._body = new St.Label({style_class: 'qtnote-sticky-body'});
+        this._body = new St.Label({
+            style_class: 'qtnote-sticky-body',
+            y_align: St.Align.START,
+            y_expand: true,
+        });
         this._body.clutter_text.line_wrap = true;
         this._body.clutter_text.line_wrap_mode = Pango.WrapMode.WORD_CHAR;
         this._body.clutter_text.ellipsize = Pango.EllipsizeMode.END;
-        this._box.add_actor(this._body);
+        this._content.add_actor(this._body);
+        this._box.add_actor(this._content);
+
+        this._resizeGrip = new St.DrawingArea({
+            reactive: true,
+            x_align: St.Align.END,
+            y_align: St.Align.END,
+            translation_x: GRIP_TRANSLATION_X,
+            translation_y: GRIP_TRANSLATION_Y,
+            style_class: 'qtnote-sticky-resize-grip',
+        });
+        this._resizeGrip.set_size(18, 18);
+        this._resizeGrip.connect('repaint', Lang.bind(this, this._paintResizeGrip));
+        this._resizeGrip.connect('button-press-event', Lang.bind(this, this._startResize));
+        this._gripLayer = new St.Bin({
+            reactive: false,
+            x_align: St.Align.END,
+            y_align: St.Align.END,
+            style_class: 'qtnote-sticky-grip-layer',
+        });
+        this._gripLayer.set_child(this._resizeGrip);
+        this._box.add_actor(this._gripLayer);
         this.setContent(this._box);
+        this._applySize();
 
         let editItem = new PopupMenu.PopupIconMenuItem(_('Edit Note'), 'document-edit-symbolic', St.IconType.SYMBOLIC);
         editItem.connect('activate', Lang.bind(this, this._open));
@@ -112,6 +162,79 @@ class QtNoteStickyDesklet extends Desklet.Desklet {
         this._body.text = message;
     }
 
+    _applySize() {
+        if (this._box)
+            this._box.set_size(this.width, this.height);
+    }
+
+    _paintResizeGrip(area) {
+        const cr = area.get_context();
+        const [width, height] = area.get_surface_size();
+        const color = area.get_theme_node().get_foreground_color();
+        Clutter.cairo_set_source_color(cr, color);
+        cr.setLineWidth(1.5);
+        for (const radius of [5, 9, 13]) {
+            cr.arc(width, height, radius, Math.PI, Math.PI * 1.5);
+            cr.stroke();
+        }
+        cr.$dispose();
+    }
+
+    _startResize(actor, event) {
+        if (event.get_button() !== Clutter.BUTTON_PRIMARY)
+            return Clutter.EVENT_PROPAGATE;
+
+        let [x, y] = event.get_coords();
+        this._resizeStart = {x: x, y: y, width: this.width, height: this.height};
+        this._draggable.inhibit = true;
+        this._resizeEventsGrabbed = Main.pushModal(this._resizeEventActor);
+        this._resizeDevice = event.get_device();
+        if (this._resizeEventsGrabbed)
+            this._resizeDevice.grab(this._resizeEventActor);
+        return Clutter.EVENT_STOP;
+    }
+
+    _handleResizeEvent(actor, event) {
+        if (!this._resizeStart)
+            return Clutter.EVENT_PROPAGATE;
+        if (event.type() === Clutter.EventType.MOTION) {
+            let [x, y] = event.get_coords();
+            let width = Math.max(220, Math.round(this._resizeStart.width + x - this._resizeStart.x));
+            let height = Math.max(140, Math.round(this._resizeStart.height + y - this._resizeStart.y));
+            this._box.set_size(width, height);
+            return Clutter.EVENT_STOP;
+        }
+        if (event.type() === Clutter.EventType.BUTTON_RELEASE) {
+            let [x, y] = event.get_coords();
+            this.width = Math.max(220, Math.round(this._resizeStart.width + x - this._resizeStart.x));
+            this.height = Math.max(140, Math.round(this._resizeStart.height + y - this._resizeStart.y));
+            this._stopResize();
+            this._applySize();
+            return Clutter.EVENT_STOP;
+        }
+        if (event.type() === Clutter.EventType.KEY_PRESS && event.get_key_symbol() === Clutter.KEY_Escape) {
+            this._stopResize();
+            this._applySize();
+            return Clutter.EVENT_STOP;
+        }
+        return Clutter.EVENT_PROPAGATE;
+    }
+
+    _stopResize() {
+        if (this._resizeDevice) {
+            if (this._resizeEventsGrabbed)
+                this._resizeDevice.ungrab();
+            this._resizeDevice = null;
+        }
+        if (this._resizeEventsGrabbed) {
+            Main.popModal(this._resizeEventActor);
+            this._resizeEventsGrabbed = false;
+        }
+        if (this._draggable)
+            this._draggable.inhibit = false;
+        this._resizeStart = null;
+    }
+
     _open() {
         if (this._stickyId)
             this._proxy.openStickyNoteRemote(this._stickyId, function() {});
@@ -128,6 +251,15 @@ class QtNoteStickyDesklet extends Desklet.Desklet {
     }
 
     on_desklet_removed(deleteConfig) {
+        this._stopResize();
+        if (this._resizeEventId) {
+            this._resizeEventActor.disconnect(this._resizeEventId);
+            this._resizeEventId = 0;
+        }
+        if (this._resizeEventActor) {
+            this._resizeEventActor.destroy();
+            this._resizeEventActor = null;
+        }
         if (this._signalId)
             this._proxy.disconnectSignal(this._signalId);
         if (this._ownerSignalId)
